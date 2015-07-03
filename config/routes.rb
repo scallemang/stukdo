67e74d4fdc27e779e8b015df372532f1f8370d9d
Sam Allemang <sca@scallemang.ca>
2015-07-03 19:21:44 -0400

    Applied new design to old task logic.

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'about' => 'pages#about'
+  get 'test' => 'pages#test'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
